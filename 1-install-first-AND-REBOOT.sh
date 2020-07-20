f12aabdfe0bea661fbe2063145ab1d6ba61359b4
felipendc <felipendc10@gmail.com>
2020-07-19 21:28:57 -0300

    Vicyos Rolling Update: (Sun 19 Jul 2020 09:28:57 PM -03)

--- 1-install-first-AND-REBOOT.sh
+++ 1-install-first-AND-REBOOT.sh
@@ -11,5 +11,11 @@ sudo apt autoremove
 # Pkgs for personal use:
 sudo apt install -y git snapd aria2 aptitude exfat-fuse exfat-utils
 
+# Codecs for Pop!_OS:
+sudo apt install libdvd-pkg
+sudo dpkg-reconfigure libdvd-pkg
+sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+
+
 # Reboot the computer
 sudo reboot
